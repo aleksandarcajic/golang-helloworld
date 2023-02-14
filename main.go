59748bb50b11fa5319828bc5ef7a27e5838d6495
aleksandarcajic <cajicaleksandar@gmail.com>
2023-02-14 11:49:23 +0100

    learning

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+var whatToSay string = "Goodbye, cruel world"
+
 func main() {
 	fmt.Println("Hello World!")
+	fmt.Println(whatToSay)
+	fmt.Println(saySomething())
+
+	changeUsingPointer(&whatToSay)
+	fmt.Println(whatToSay)
+}
+
+func saySomething() int {
+	return 10
+}
+
+func changeUsingPointer(s *string) {
+	*s = "Hello again!!!"
 }
